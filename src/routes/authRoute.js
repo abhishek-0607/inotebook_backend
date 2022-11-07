1edebd7b64707529e6ceda44e1999ca282a2387a
Abhishek Silawat <absilawat@gmail.com>
2022-11-07 17:11:51 +0530

    create User or Tegister functionality

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -4,31 +4,30 @@ const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
 
 //create a user using: POST "/api/auth/"
-router.post(
-  "/register",
-  [
-    body("name", "enter a valid name").isLength({ min: 3 }),
-    body("email", "enter a valid email").isEmail(),
-    body("password", "password must be atleast 5 characters").isLength({
-      min: 5,
-    }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      let user = await User.findOne({ email: req.body.email });
-
-      if (user) {
-        res.status(400).json("user with this email already exists");
-      }
-      user = await User.create(req.body);
-      return res.status(200).json(user);
-    } catch (e) {
-      return res.status(500).json({ message: e.message, status: "failed" });
-    }
+const createUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      res.status(400).json("user with this email already exists");
+    }
+    user = await User.create(req.body);
+    return res.status(200).json(user);
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: "failed" });
+  }
+};
+router.post("/register", [
+  body("name", "enter a valid name").isLength({ min: 3 }),
+  body("email", "enter a valid email").isEmail(),
+  body("password", "password must be atleast 5 characters").isLength({
+    min: 5,
+  }),
+  createUser,
+]);
+
 module.exports = router;
