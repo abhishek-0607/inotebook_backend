3a9472f3984d12e099fe1783071d18d5bbedfb57
Abhishek Silawat <absilawat@gmail.com>
2022-11-11 23:04:15 +0530

    created login route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //create a user using: POST "/api/auth/"
 const createUser = async (req, res) => {
@@ -15,19 +18,65 @@ const createUser = async (req, res) => {
     if (user) {
       res.status(400).json("user with this email already exists");
     }
-    user = await User.create(req.body);
-    return res.status(200).json(user);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword,
+    });
+    const token = jwt.sign(user.id, process.env.JWT_SECRET);
+    return res.status(200).json({ token, user });
   } catch (e) {
     return res.status(500).json({ message: e.message, status: "failed" });
   }
 };
-router.post("/register", [
-  body("name", "enter a valid name").isLength({ min: 3 }),
-  body("email", "enter a valid email").isEmail(),
-  body("password", "password must be atleast 5 characters").isLength({
-    min: 5,
-  }),
-  createUser,
-]);
+
+router.post(
+  "/register",
+  [
+    body("name", "enter a valid name").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  createUser
+);
+
+const loginUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Incorrect Credentials" });
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.status(400).json({ error: "Incorrect Credentials" });
+    }
+
+    const token = jwt.sign(user.id, process.env.JWT_SECRET);
+    return res.status(200).json({ token, user });
+  } catch {
+    return res.status(500).json({ message: e.message, status: "failed" });
+  }
+};
+
+router.post(
+  "/login",
+  [
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password is required").exists(),
+  ],
+  loginUser
+);
 
 module.exports = router;
